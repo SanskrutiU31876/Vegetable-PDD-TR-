666ea071d126b4c7fc6aa38d721f767316ea37a6
SanskrutiU31876 <156110161+SanskrutiU31876@users.noreply.github.com>
2024-03-16 19:24:11 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,8 +8,8 @@ import base64
 import cv2
 
 # Load models and class names
-#MODEL = tf.keras.models.load_model('./potato_trained_models/1/')
-MODEL = tf.keras.models.load_model('tomato.h5')
+MODEL = tf.keras.models.load_model('./potato_trained_models/1/')
+#MODEL = tf.keras.models.load_model('tomato.h5')
 TOMATO_MODEL = tf.keras.models.load_model('./tomato_trained_models/1')
 #TOMATO_MODEL = tf.keras.models.load_model('tomato.h5')
 PEPPER_MODEL = tf.keras.models.load_model('./pepper_trained_models/1')
