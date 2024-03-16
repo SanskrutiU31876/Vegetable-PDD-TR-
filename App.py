d26496349fe4c215beaacc1789e799cd4d830af7
SanskrutiU31876 <156110161+SanskrutiU31876@users.noreply.github.com>
2024-03-16 19:35:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,9 +8,9 @@ import base64
 import cv2
 
 # Load models and class names
-MODEL = tf.keras.models.load_model('./potato_trained_models/1')
+MODEL = tf.keras.models.load_model('potatoes.h5')
 #MODEL = tf.keras.models.load_model('tomato.h5')
-#TOMATO_MODEL = tf.keras.models.load_model('./tomato_trained_models/1')
+TOMATO_MODEL = tf.keras.models.load_model('./tomato_trained_models/1')
 TOMATO_MODEL = tf.keras.models.load_model('tomato.h5')
 PEPPER_MODEL = tf.keras.models.load_model('./pepper_trained_models/1')
 class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
